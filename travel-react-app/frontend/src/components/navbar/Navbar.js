b0a5095d0633c3eacc7720eeb1ee4f7d80169ff7
Radu Vlad <mihairadu785@yahoo.com>
2024-03-16 15:12:03 +0200

    Update la cod

--- travel-react-app/frontend/src/components/navbar/Navbar.js
+++ travel-react-app/frontend/src/components/navbar/Navbar.js
@@ -2,6 +2,11 @@ import React from 'react'
 import {BiSearch} from 'react-icons/bi'
 import {BsPerson} from 'react-icons/bs'
 import{HiOutlineMenuAlt4} from 'react-icons/hi'
+import{FaFacebook} from'react-icons/fa'
+import{FaInstagram} from'react-icons/fa'
+import{FaTwitter} from'react-icons/fa'
+import{FaPinterest} from'react-icons/fa'
+import{FaYouTube} from'react-icons/fa'
 
 import'./NavbarStyles.css'
 function Navbar() {
@@ -26,7 +31,30 @@ function Navbar() {
            <HiOutlineMenuAlt4 className='icon' />
         </div>
 
+        <div className="mobile-menu">
+            <ul className="mobile-nav">
+            <li>Home</li>
+        <li>Destinations</li>
+        <li>Travel</li>
+        <li>Book</li>
+        <li>Views</li>
+            </ul>
+            <div className="mobile-menu-botton">
+                <div className="menu-icons">
+                <button>Search</button>
+                <button>Account</button>
+            </div>
+            <div className="social-icons"></div>
+            <FaFacebook className='icon' />
+            <FaInstagram className='icon' />
+            <FaTwitter className='icon' />
+            <FaPinterest className='icon' />
+            <FaYouTube className='icon' />
         </div>
+
+        </div>
+        </div>
+
     )
 }
 export default Navbar
