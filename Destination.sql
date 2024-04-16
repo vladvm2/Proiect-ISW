0be75ad57e39e212c7c608f5ff5b5dbb6c484211
Iulian Bobocea <108735417+BrokN1@users.noreply.github.com>
2024-04-16 22:03:06 +0300

    Delete Destination.sql

--- Destination.sql
+++
@@ -1,8 +0,0 @@
-use Website;
-create Table Destination(
-DestinationID INT PRIMARY KEY,
-Name NVARCHAR(50) NOT NULL,
-Description NVARCHAR(200) NOT NULL,
-Location NVARCHAR(50) NOT NULL
-)
-select * from Destination
