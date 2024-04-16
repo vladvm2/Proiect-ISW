a85db644c96cc8dd1d686972176bb81520d1af9a
Iulian Bobocea <108735417+BrokN1@users.noreply.github.com>
2024-04-16 22:03:00 +0300

    Delete Card.sql

--- Card.sql
+++
@@ -1,8 +0,0 @@
-use Website;
-create Table Card(
-CardNumber INT PRIMARY KEY ,
-ExpirationDate DATE NOT NULL,
-SecurityCode INT NOT NULL,
-UserName NVARCHAR(100) NOT NULL FOREIGN KEY (UserName) REFERENCES [User](UserName)
-);
-select * from Card
