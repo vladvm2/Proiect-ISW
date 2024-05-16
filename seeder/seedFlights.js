ef6963ae3a82e0edd434447848529a65456e7107
Iulian Bobocea <108735417+BrokN1@users.noreply.github.com>
2024-05-16 21:50:50 +0300

    Update seedFlights.js

--- seeder/seedFlights.js
+++ seeder/seedFlights.js
@@ -4,65 +4,57 @@ const seedData = [
   {
     departure: 'Bucuresti',
     arrival: 'Paris',
-    departureDateTime: new Date(2024, 3, 20, 7, 0),
-    arrivalDateTime: new Date(2024, 3, 20, 10, 0),
+    departureDateTime: new Date(2024, 4, 20, 7, 0),
+    arrivalDateTime: new Date(2024, 4, 20, 10, 0),
     price: 400,
     seatsAvailable: 150,
   },
   {
     departure: 'Bucuresti',
     arrival: 'Paris',
-    departureDateTime: new Date(2024, 3, 20, 9, 30), // April 20, 2024, 09:30 AM
-    arrivalDateTime: new Date(2024, 3, 20, 12, 30), // April 20, 2024, 12:30 PM
+    departureDateTime: new Date(2024, 4, 20, 9, 30), // April 20, 2024, 09:30 AM
+    arrivalDateTime: new Date(2024, 4, 20, 12, 30), // April 20, 2024, 12:30 PM
     price: 450,
     seatsAvailable: 130,
   },
   {
     departure: 'Bucuresti',
     arrival: 'Paris',
-    departureDateTime: new Date(2024, 3, 20, 12, 15), // April 20, 2024, 12:15 PM
-    arrivalDateTime: new Date(2024, 3, 20, 15, 15), // April 20, 2024, 03:15 PM
+    departureDateTime: new Date(2024, 4, 20, 12, 15), // April 20, 2024, 12:15 PM
+    arrivalDateTime: new Date(2024, 4, 20, 15, 15), // April 20, 2024, 03:15 PM
     price: 380,
     seatsAvailable: 140,
   },
   {
     departure: 'Cluj-Napoca',
     arrival: 'Londra',
-    departureDateTime: new Date(2024, 3, 21, 9, 0),
-    arrivalDateTime: new Date(2024, 3, 21, 11, 30),
+    departureDateTime: new Date(2024, 4, 21, 9, 0),
+    arrivalDateTime: new Date(2024, 4, 21, 11, 30),
     price: 350,
     seatsAvailable: 130,
   },
   {
     departure: 'Iasi',
     arrival: 'Berlin',
-    departureDateTime: new Date(2024, 3, 22, 13, 0),
-    arrivalDateTime: new Date(2024, 3, 22, 15, 30),
+    departureDateTime: new Date(2024, 4, 22, 13, 0),
+    arrivalDateTime: new Date(2024, 4, 22, 15, 30),
     price: 300,
     seatsAvailable: 120,
   },
   {
     departure: 'Timisoara',
     arrival: 'Viena',
-    departureDateTime: new Date(2024, 3, 21, 6, 0),
-    arrivalDateTime: new Date(2024, 3, 21, 7, 0),
+    departureDateTime: new Date(2024, 4, 21, 6, 0),
+    arrivalDateTime: new Date(2024, 4, 21, 7, 0),
     price: 250,
     seatsAvailable: 140,
   },
   {
-    departure: 'Paris',
-    arrival: 'Napoli',
-    departureDateTime: new Date(2024, 3, 21, 15, 0),
-    arrivalDateTime: new Date(2024, 3, 21, 18, 0),
-    price: 450,
-    seatsAvailable: 100,
-  },
-  {
     departure: 'Constanta',
     arrival: 'Madrid',
-    departureDateTime: new Date(2024, 3, 17, 15, 0),
-    arrivalDateTime: new Date(2024, 3, 17, 18, 0),
-    price: 400,
+    departureDateTime: new Date(2024, 4, 22, 15, 0),
+    arrivalDateTime: new Date(2024, 4, 22, 18, 0),
+    price: 450,
     seatsAvailable: 100,
   },
 ]
